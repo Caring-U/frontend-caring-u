128637309b3fe0cc95e16b3308fe21441830d5b9
unknown <agit.zaini@gmail.com>
2022-07-07 14:37:38 +0800

    UI profile & dashboard psikolog

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import HomePagePsycolog from "./pages/HomePagePsycolog";
+import ProfilePagePsycolog from "./pages/ProfilePagePsycholog";
 export default function App() {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/psikolog/:id" element={<ProfilePage />} />
             <Route path="/psycolog/" element={<HomePagePsycolog />} />
+            <Route path="/psycolog/psycologProfile" element={<ProfilePagePsycolog />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
         </Routes>
--- src/pages/HomePagePsycolog.js
+++ src/pages/HomePagePsycolog.js
@@ -2,6 +2,17 @@ export default function HomePagePsycolog() {
     return (
         <>
             <main>
+                <div className="flex justify-center py-2 bg-sky-600">
+                    <div className="w-3/4 flex justify-between items-center">
+                        <img src="/foto/logo.png" alt="logo" className="w-28" />
+                        <div className="flex gap-4 text-white">
+                            <a href="#">Menu 1</a>
+                            <a href="#">Menu 2</a>
+                            <a href="#">Menu 3</a>
+                            <a href="#">Menu 4</a>
+                        </div>
+                    </div>
+                </div>
                 <div
                     className="relative pt-16 pb-32 flex content-center items-center justify-center"
                     style={{
@@ -21,9 +32,7 @@ export default function HomePagePsycolog() {
                             <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
                                 <div className="pr-12">
                                     <h1 className="text-white font-semibold text-5xl">Welcome To Caring-U Homepage.</h1>
-                                    <p className="mt-4 text-lg text-gray-300">
-                                        This is a simple example of a Landing Page you can build using Tailwind Starter Kit. It features multiple CSS components based on the Tailwindcss design system.
-                                    </p>
+                                    <p className="mt-4 text-lg text-gray-300">This is a simple of a Landing Page you can build using Tailwind Starter Kit. It features multiple CSS components based on the Tailwindcss design system.</p>
                                 </div>
                             </div>
                         </div>
@@ -41,25 +50,92 @@ export default function HomePagePsycolog() {
                             <div className="lg:pt-12 pt-6 w-full md:w-5/6 px-4 text-center">
                                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                                     <div className="px-4 py-5 flex-auto">
-                                        <table className="table-auto">
-                                            <thead>
-                                                <tr>
-                                                    <th>1</th>
-                                                    <th>2</th>
-                                                    <th>3</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>asdas</td>
-                                                    <td>asdasasfa</td>
-                                                    <td>asdasasfaasdas</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                        <h6 className="text-xl font-semibold">Awarded Agency</h6>
+                                        <h6 className="text-xl font-semibold">Schedule Konsultant</h6>
                                         <p className="mt-2 mb-4 text-gray-600">Divide details about your product or agency work into parts. A paragraph describing a feature will be enough.</p>
                                     </div>
+                                    <table className="table-auto border-separate border-spacing-2 border border-slate-400">
+                                        <thead>
+                                            <tr>
+                                                <th className="border border-slate-300">Day</th>
+                                                <th className="border border-slate-300">Date</th>
+                                                <th className="border border-slate-300">Time</th>
+                                                <th className="border border-slate-300">Status</th>
+                                                <th className="border border-slate-300">Room</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td className="border border-slate-300">Today</td>
+                                                <td className="border border-slate-300">12-July-2022</td>
+                                                <td className="border border-slate-300">06:00 PM</td>
+                                                <td className="border border-slate-300 font-bold uppercase text-xs ">Scheduled</td>
+                                                <td className="border border-slate-300">
+                                                    <button
+                                                        class="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-1 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                        type="button"
+                                                    >
+                                                        Join
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td className="border border-slate-300">Friday</td>
+                                                <td className="border border-slate-300">13-July-2022</td>
+                                                <td className="border border-slate-300">06:00 PM</td>
+                                                <td className="border border-slate-300 font-bold uppercase text-xs ">Scheduled</td>
+                                                <td className="border border-slate-300">
+                                                    <button
+                                                        class="bg-gray-500 text-white active:bg-gray-600 font-bold uppercase text-xs px-4 py-1 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                        type="button"
+                                                    >
+                                                        Waiting
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td className="border border-slate-300">Saturday</td>
+                                                <td className="border border-slate-300">14-July-2022</td>
+                                                <td className="border border-slate-300">06:00 PM</td>
+                                                <td className="border border-slate-300 font-bold uppercase text-xs ">Free Booking</td>
+                                                <td className="border border-slate-300"></td>
+                                            </tr>
+                                            <tr>
+                                                <td className="border border-slate-300">Thursday</td>
+                                                <td className="border border-slate-300">12-July-2022</td>
+                                                <td className="border border-slate-300">06:00 PM</td>
+                                                <td className="border border-slate-300 font-bold uppercase text-xs ">Scheduled</td>
+                                                <td className="border border-slate-300">
+                                                    <button
+                                                        class="bg-gray-500 text-white active:bg-gray-600 font-bold uppercase text-xs px-4 py-1 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                        type="button"
+                                                    >
+                                                        Waiting
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td className="border border-slate-300">Friday</td>
+                                                <td className="border border-slate-300">13-July-2022</td>
+                                                <td className="border border-slate-300">06:00 PM</td>
+                                                <td className="border border-slate-300 font-bold uppercase text-xs ">Scheduled</td>
+                                                <td className="border border-slate-300">
+                                                    <button
+                                                        class="bg-gray-500 text-white active:bg-gray-600 font-bold uppercase text-xs px-4 py-1 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                        type="button"
+                                                    >
+                                                        Waiting
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td className="border border-slate-300">Saturday</td>
+                                                <td className="border border-slate-300">14-July-2022</td>
+                                                <td className="border border-slate-300">06:00 PM</td>
+                                                <td className="border border-slate-300 font-bold uppercase text-xs ">Free Booking</td>
+                                                <td className="border border-slate-300"></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
