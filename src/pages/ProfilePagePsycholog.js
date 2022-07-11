12deace983c0aa62db3fc565f756d83c70baa9d1
unknown <agit.zaini@gmail.com>
2022-07-11 22:44:16 +0800

    setting schedule

--- src/pages/ProfilePagePsycholog.js
+++ src/pages/ProfilePagePsycholog.js
@@ -1,4 +1,8 @@
+import { React, useState } from "react";
+
 export default function ProfilePagePsycolog() {
+    const [showModal, setShowModal] = useState(false);
+    const [date, setDate] = useState();
     return (
         <>
             <main className="profile-page">
@@ -44,6 +48,7 @@ export default function ProfilePagePsycolog() {
                                                 className="bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
                                                 type="button"
                                                 style={{ transition: "all .15s ease" }}
+                                                onClick={() => setShowModal(true)}
                                             >
                                                 Setting Schedule
                                             </button>
@@ -83,6 +88,59 @@ export default function ProfilePagePsycolog() {
                     </div>
                 </section>
             </main>
+            {showModal ? (
+                <>
+                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                            {/*content*/}
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                    <h3 className="text-3xl font-semibold uppercase">Setting Schedule</h3>
+                                    <button className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onClick={() => setShowModal(false)}>
+                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">×</span>
+                                    </button>
+                                </div>
+                                {/*body*/}
+                                <div className="relative py-3 px-4 flex-auto">
+                                    {/* body/dropdown */}
+                                    <button className="relative flex jutify-center items-center bg-white text-gray-600 rounded focus:outline-none focus:ring ring-gray-200 border shadow group">
+                                        <p className="px-4">Selected Date: {date}</p>
+                                        <input type="date" onChange={(e) => setDate(e.target.value)} />
+                                    </button>
+                                    {/* body/time */}
+                                </div>
+                                <div className="relative py-3 px-4 flex-auto">
+                                    {/* body/dropdown */}
+                                    <button className="relative flex jutify-center items-center bg-white text-gray-600 rounded focus:outline-none focus:ring ring-gray-200 border shadow group">
+                                        <p className="px-4">Selected Date: {date}</p>
+                                        <input type="date" onChange={(e) => setDate(e.target.value)} />
+                                    </button>
+                                    {/* body/time */}
+                                </div>
+                                {/*footer*/}
+                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                    <button
+                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Close
+                                    </button>
+                                    <button
+                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Save Changes
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+            ) : null}
         </>
     );
 }
